d9ff662074c2a88c2fa8cc5450ae44b132a78a44
Peter Danenberg <pcd@roxygen.org>
2011-05-04 10:46:20 +0000

    Remove superfluous classes.
    git-svn-id: svn://scm.r-forge.r-project.org/svnroot/rjavax/pkg@50 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- src/TestCallableReturn.java
+++ src/TestCallableReturn.java
@@ -1,28 +1,13 @@
 
-import java.lang.reflect.InvocationHandler;
-import java.lang.reflect.Method;
-import java.lang.reflect.Proxy;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
-
 import org.rosuda.REngine.RList;
 import org.rosuda.REngine.REngine;
 import org.rosuda.REngine.REXP;
-import org.rosuda.REngine.REXPDouble;
-import org.rosuda.REngine.REXPInteger;
 import org.rosuda.REngine.REXPEnvironment;
-import org.rosuda.REngine.REXPFactor;
 import org.rosuda.REngine.REXPLanguage;
-import org.rosuda.REngine.REXPList;
-import org.rosuda.REngine.REXPRaw;
 import org.rosuda.REngine.REXPReference;
 import org.rosuda.REngine.REXPString;
 import org.rosuda.REngine.REXPUnknown;
-import org.rosuda.REngine.REXPWrapper;
 import org.rosuda.REngine.REXPJavaReference;
-import org.rosuda.REngine.JRI.JRIEngine;
 
 public class TestCallableReturn {
     public static void test(REXPReference dollars,
